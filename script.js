c0cdc7e2673360ce4cbef0b89ccf7aedcdc2911c
sapar6ek0v <razetka0309@gmail.com>
2022-12-19 13:53:03 +0300

    fix

--- script.js
+++ script.js
@@ -1,281 +1,285 @@
-const add = document.querySelector('.add')
-const startBtn = document.querySelector('.start-button')
-const container = document.querySelector('.container')
-const nextBtn = document.querySelector('.button')
-const minLive = document.querySelector('.live')
-const currentLev = document.querySelector('.current-level')
-const difficult = document.querySelector('#checkbox')
-const balls = document.querySelector('.balls')
-const list = document.querySelector('#list')
-// const valList = document.createElement('li')
-// list.append(valList)
-balls.textContent = `Ваши баллы:`
+const container = document.querySelector('.container');
 
-let points = 0
-let curLevel = 0
-let level = 0
-let live = 3
-let gamePaused = true
-let counts = 0
-let levelCounts = 0
-minLive.textContent = `LIVE ${live}`
+const startBtn = document.querySelector('.start__btn');
+const nextBtn = document.querySelector('.next__btn');
 
+const minLive = document.querySelector('.navbar__live-title');
+const points = document.querySelector('.navbar__points');
+const list = document.querySelector('.navbar__list');
+const difficult = document.querySelector('.select__options');
+
+//TODO:
+const add = document.querySelector('.remember-way__container');
+const currentLev = document.querySelector('.remember-way__current-level');
+
+let allPoints = 0;
+let curLevel = 0;
+let lives = 3;
+let counts = 0;
+let levelCounts = 0;
+let isGamePaused = true;
+
+points.textContent = 'Your points: 0';
+minLive.textContent = `Your lives: ${lives}`;
 
 const level1 = [
-    [2,0,0,0,0],
-    [1,1,1,0,0],
-    [0,0,1,0,0],
-    [0,0,1,1,1],
-    [0,0,0,0,3],
-]
+  [2, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 1, 1],
+  [0, 0, 0, 0, 3],
+];
+
 const level2 = [
-    [0,1,1,1,2,0],
-    [0,1,0,0,0,0],
-    [0,1,1,1,0,0],
-    [0,0,0,1,0,0],
-    [0,1,1,1,0,0],
-    [0,3,0,0,0,0],
-]
+  [0, 1, 1, 1, 2, 0],
+  [0, 1, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 1, 0, 0],
+  [0, 1, 1, 1, 0, 0],
+  [0, 3, 0, 0, 0, 0],
+];
+
 const level3 = [
-    [0,2,1,1,1,1,1],
-    [0,0,0,0,0,0,1],
-    [0,0,0,0,1,1,1],
-    [0,0,1,1,1,0,0],
-    [0,0,1,0,0,0,0],
-    [0,0,1,0,0,0,0],
-    [0,0,1,1,1,1,3],
-]
+  [0, 2, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 1, 1, 1],
+  [0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 3],
+];
+
 const level4 = [
-    [0,0,0,0,0,2,1,0],
-    [0,0,0,0,0,0,1,0],
-    [1,1,1,1,1,1,1,0],
-    [1,0,0,0,0,0,0,0],
-    [1,1,1,0,0,0,0,0],
-    [0,0,1,1,1,1,1,0],
-    [0,0,0,0,0,0,1,0],
-    [0,0,3,1,1,1,1,0],
-]
+  [0, 0, 0, 0, 0, 2, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0],
+  [1, 1, 1, 1, 1, 1, 1, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 3, 1, 1, 1, 1, 0],
+];
+
 const level5 = [
-    [2,1,0,0,0,0,0,0,0],
-    [0,1,1,1,1,0,0,0,0],
-    [0,0,0,0,1,0,0,0,0],
-    [0,0,1,1,1,0,0,0,0],
-    [0,0,1,0,0,0,0,0,0],
-    [0,0,1,1,1,1,1,0,0],
-    [0,0,0,0,0,0,1,0,0],
-    [0,1,1,1,1,1,1,0,0],
-    [0,3,0,0,0,0,0,0,0],
-]
+  [2, 1, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 1, 1, 1, 1, 1, 0, 0],
+  [0, 3, 0, 0, 0, 0, 0, 0, 0],
+];
 
 const levels = {
-    0: level1,
-    1: level2,
-    2: level3,
-    3: level4,
-    4: level5,
-}
-const countsPoints = () => {
-    if ( counts > points ) {
-        points = counts
-        list.textContent = `Лучший результат ${ points }`
-    }
-}
+  0: level1,
+  1: level2,
+  2: level3,
+  3: level4,
+  4: level5,
+};
 
-const clearAdd = () => {
-    add.innerHTML = ''
-}
-const levelUp = (e) => {
-    if ( counts > 20 && confirm('Хотите ли обменять ваши 20 баллы на 1 жизнь')) {
-        counts -= 20
-        live += 1
-        minLive.textContent = `LIVE: ${live}`
-        balls.textContent =`ВАШИ БАЛЛЫ: ${counts}`
-    }
-    e.stopPropagation()
-    clearAdd()
-    level++
-    add.innerHTML = `<div class="text-content">Уровень ${level} пройден !</div>`
-    nextBtn.hidden = false
-    startBtn.hidden = true
-    // container.addEventListener('mouseover', gameOver)
-    curLevel++
-    levelCounts = 0
-    gamePaused = true
-    // if  ( live < 3 || curLevel > 0 ) {
-    //     difficult.disabled = true
-    // }
-    // valList.textContent = `Лучший результат: ${counts += +difficult.value}`
-    if ( curLevel + 2 ) {
-        live += 1
-    }
+const pointCounter = () => {
+  if (counts > allPoints) {
+    allPoints = counts;
+    list.textContent = `Best result: ${allPoints}`;
+  }
+};
 
+const handleClearWay = () => {
+  add.innerHTML = '';
+};
 
-}
+const levelUp = (event) => {
+  event.stopPropagation();
+  if (counts > 20 && confirm('Do you want to exchange your 20 points for 1 life?')) {
+    counts -= 20;
+    lives += 1;
+    minLive.textContent = `Your lives: ${lives}`;
+    points.textContent = `Your points: ${counts}`;
+  }
+
+  handleClearWay();
+  curLevel++;
+  add.innerHTML = `<div class="remember-way__level-title">Level ${curLevel} passed successfully!</div>`;
+  nextBtn.hidden = false;
+  startBtn.hidden = true;
+  levelCounts = 0;
+  isGamePaused = true;
+  // if  ( lives < 3 || curLevel > 0 ) {
+  //     difficult.disabled = true
+  // }
+  // if (curLevel + 2) {
+  //   lives += 1;
+  // }
+};
+
 const startGame = () => {
-    gamePaused = false
-    Array.from(add.children).forEach(it => {
-        it.classList.remove('close')
-        it.classList.remove('open')
-    })
-    document.querySelector('.begin').addEventListener('mouseover', () => {
-        document.querySelector('.finish').addEventListener('mouseover', levelUp)
-    })
-}
+  isGamePaused = false;
+
+  Array.from(add.children).forEach((it) => {
+    it.classList.remove('close');
+    it.classList.remove('open');
+  });
+
+  document.querySelector('.begin').addEventListener('mouseover', () => {
+    document.querySelector('.finish').addEventListener('mouseover', levelUp);
+  });
+};
 
 const gameOver = () => {
-    if  ( live < 3 || curLevel > 0 ) {
-        difficult.disabled = true
+  if (lives < 3 || curLevel > 0) {
+    difficult.disabled = true;
+  }
+
+  if (isGamePaused) {
+    return;
+  }
+
+  counts -= levelCounts + 2;
+  levelCounts = 0;
+  lives--;
+  if (lives <= 0) {
+    pointCounter();
+    curLevel = 0;
+    lives = 3;
+    counts = 0;
+    difficult.disabled = false;
+    alert('You have to start from beginning!');
+  }
+  // if ( counts < levelCounts ) {
+  //     valList.textContent = `Лучший результат: ${counts += +difficult.value}`
+  //     valList.textContent = `${counts = levelCounts}`
+  // }
+  minLive.textContent = `Your lives: ${lives}`;
+  points.textContent = `Your points: ${counts}`;
+
+  handleClearWay();
+  fillOut();
+  isGamePaused = true;
+};
+
+const addRed = (block) => {
+  block.classList.add('close');
+  block.addEventListener('mouseover', gameOver);
+};
+
+const addGreen = (block) => {
+  block.classList.add('open');
+  block.addEventListener('mouseover', (e) => {
+    e.stopPropagation();
+    block.classList.add('open');
+  });
+  block.addEventListener('mouseover', function countPoints() {
+    if (!isGamePaused) {
+      counts += +difficult.value;
+      levelCounts += +difficult.value;
+      // add.addEventListener('mouseover', () => {
+      //     valList.innerHTML = `<div> Рейтинг очков </div>
+      //             ${counts += +difficult.value}`
+      // })
+      // valList.textContent = `Лучший результат${counts += +difficult.value}`
+      points.textContent = `Your points: ${counts}`;
+      // valList.textContent = `Лучший результат: ${counts += +difficult.value}`
+
+      block.removeEventListener('mouseover', countPoints);
+      if (difficult.checked) {
+        counts += 1;
+      }
     }
-    if (gamePaused) {
-        return ;
-    }
-    counts -= levelCounts + 2
-    levelCounts = 0
-    live--
-    if (live <=0 ) {
-        countsPoints()
-        curLevel = 0
-        live = 3
-        counts = 0
-        difficult.disabled = false
-        alert('Вы должны начать заново')
-    }
-    // if ( counts < levelCounts ) {
-    //     valList.textContent = `Лучший результат: ${counts += +difficult.value}`
-    //     valList.textContent = `${counts = levelCounts}`
-    // }
-    minLive.textContent = `LIVE ${live}`
-    balls.textContent = `Ваши баллы: ${counts}`
+  });
+};
 
-    clearAdd()
-    fillOut()
-    gamePaused =true
-}
-const addRed = block => {
-    block.classList.add('close')
-    block.addEventListener('mouseover' , gameOver)
-}
+const addStart = (block) => {
+  block.classList.add('start', 'begin');
+  block.textContent = 'S';
+  block.addEventListener('mouseover', (e) => {
+    e.stopPropagation();
+    container.addEventListener('mouseover', gameOver, { once: true });
+  });
+};
 
-const addGreen = block => {
-    block.classList.add('open')
-    block.addEventListener('mouseover', (e) => {
-        e.stopPropagation()
-        block.classList.add('open')
-    })
-    block.addEventListener('mouseover', function countPoints () {
-        if (!gamePaused) {
-            counts += +difficult.value
-            levelCounts  += +difficult.value
-            // add.addEventListener('mouseover', () => {
-            //     valList.innerHTML = `<div> Рейтинг очков </div>
-            //             ${counts += +difficult.value}`
-            // })
-            // valList.textContent = `Лучший результат${counts += +difficult.value}`
-            balls.textContent = `Ваши баллы: ${counts}`
-            // valList.textContent = `Лучший результат: ${counts += +difficult.value}`
+const addFinish = (block) => {
+  block.classList.add('start', 'finish');
+  block.textContent = 'F';
+};
 
-            block.removeEventListener('mouseover', countPoints )
-            if (difficult.checked) {
-                counts += 1
-            }
+const fillOut = () => {
+  currentLev.textContent = `Your level ${curLevel + 1}`;
+  add.innerHTML = '';
+  if (!levels[curLevel]) {
+    pointCounter();
+    alert('You passed game! Congrats!!!');
+    curLevel = 0;
+    difficult.disabled = false;
+    // lives = 3
+    // counts = 0
+    // minLive.textContent = `LIVE ${lives}`
+    // points.textContent = `Ваши баллы: ${counts}`
+    fillOut();
+    return;
+  }
+  const box = `${100 / levels[curLevel][0].length}%`;
 
-        }
-    })
-}
-
-const addStart = block => {
-    block.classList.add('start', 'begin')
-    block.textContent = 'S'
-    block.addEventListener('mouseover', (e) => {
-        e.stopPropagation()
-        container.addEventListener('mouseover', gameOver, {once:true})
-    })
-
-}
-
-const addFinish = block => {
-    block.classList.add('start', 'finish')
-    block.textContent = 'F'
-}
-
-const fillOut = () =>{
-    currentLev.textContent = `Ваш уровень ${curLevel + 1}`
-    add.innerHTML = ''
-    if (!levels[curLevel]) {
-        countsPoints()
-        alert("Ты прошёл мою игру")
-        curLevel = 0
-        difficult.disabled = false
-        // live = 3
-        // counts = 0
-        // minLive.textContent = `LIVE ${live}`
-        // balls.textContent = `Ваши баллы: ${counts}`
-        fillOut()
-        return;
-    }
-    const box = `${100 / levels[curLevel][0].length}%`
-
-    levels[curLevel].forEach( row => {
-        row.forEach( it => {
-            const block = document.createElement('div')
-            block.style.width = box
-            block.style.height = box
-            block.classList.add('square')
-            if ( difficult.value !== '2' && difficult.value !== '4' ) {
-                block.classList.add('square-border')
-            }
-            if ( it === 0 ) {
-                addRed(block)
-            } else if ( it === 1 ) {
-                addGreen(block)
-            } else if ( it === 2 ) {
-               addStart(block)
-            } else if ( it === 3 ) {
-                addFinish(block)
-            }
-            add.append(block)
-        })
-    })
-    Array.from(document.querySelectorAll('.close')).forEach( it => {
-        it.addEventListener('mouseover', () => {
-            document.querySelector('.finish').removeEventListener('mouseover', levelUp)
-        })
-    })
-
-}
-fillOut()
+  levels[curLevel].forEach((row) => {
+    row.forEach((it) => {
+      const block = document.createElement('div');
+      block.style.width = box;
+      block.style.height = box;
+      block.classList.add('square');
+      if (difficult.value !== '2' && difficult.value !== '4') {
+        block.classList.add('square-border');
+      }
+      if (it === 0) {
+        addRed(block);
+      } else if (it === 1) {
+        addGreen(block);
+      } else if (it === 2) {
+        addStart(block);
+      } else if (it === 3) {
+        addFinish(block);
+      }
+      add.append(block);
+    });
+  });
+  Array.from(document.querySelectorAll('.close')).forEach((it) => {
+    it.addEventListener('mouseover', () => {
+      document.querySelector('.finish').removeEventListener('mouseover', levelUp);
+    });
+  });
+};
+fillOut();
 
 startBtn.addEventListener('click', () => {
-   startGame()
-})
+  startGame();
+});
+
 nextBtn.addEventListener('click', () => {
-    fillOut()
-    startBtn.hidden = false
-    nextBtn.hidden = true
-    // nextBtn.addEventListener('click', ()=> {
-    //     if (levels[curLevel][-1]) {
-    //         clearAdd()
-    //         fillOut()
-    //     }
-    // })
-})
+  fillOut();
+  startBtn.hidden = false;
+  nextBtn.hidden = true;
+  // nextBtn.addEventListener('click', ()=> {
+  //     if (levels[curLevel][-1]) {
+  //         handleClearWay()
+  //         fillOut()
+  //     }
+  // })
+});
 
 difficult.addEventListener('change', () => {
-    const squares = Array.from(document.querySelectorAll('.square'))
-    if ( difficult.value === '1' ) {
-        add.classList.remove('cursor')
-        squares.forEach(it => it.classList.add('square-border'))
-    } else if ( difficult.value === '2' ) {
-        add.classList.remove('cursor')
-        squares.forEach(it => it.classList.remove('square-border'))
-    } else if ( difficult.value === '3' ) {
-        squares.forEach(it => it.classList.add('square-border'))
-        add.classList.add('cursor')
-    } else if ( difficult.value === '4' ) {
-        squares.forEach(it => it.classList.remove('square-border'))
-        add.classList.add('cursor')
-    }
-})
-
-
-
+  const squares = Array.from(document.querySelectorAll('.square'));
+  if (difficult.value === '1') {
+    add.classList.remove('cursor');
+    squares.forEach((it) => it.classList.add('square-border'));
+  } else if (difficult.value === '2') {
+    add.classList.remove('cursor');
+    squares.forEach((it) => it.classList.remove('square-border'));
+  } else if (difficult.value === '3') {
+    squares.forEach((it) => it.classList.add('square-border'));
+    add.classList.add('cursor');
+  } else if (difficult.value === '4') {
+    squares.forEach((it) => it.classList.remove('square-border'));
+    add.classList.add('cursor');
+  }
+});
